51425e577b8f33d5b99177e731f6808ee69dd132
Justin Aurand <justin.aurand@swiftcapital.com>
2018-01-06 18:33:21 -0500

    Added object tracker parser guy

---
+++ ObjectTrackerParser.java
@@ -0,0 +1,40 @@
+public class ObjectTrackerParser {
+    // Ignore this
+    public static void main(String[] args) throws Exception {
+        ObjectTrackerParser parser = new ObjectTrackerParser();
+
+        try {
+            parser.run(args);
+        } catch(Exception ex) {
+            throw (ex);
+        }
+        
+    }
+
+    public void run (String[] args) throws Exception
+    {
+        // Ignore this
+        Jevois jevois = new Jevois();
+
+        // Read in the Jevois string
+        String jevoisString = jevois.readString();
+
+        // Parse the information
+        String[] inputs = jevoisString.split(" ");
+
+        // Display it
+        int x = Integer.parseInt(inputs[1]);
+        int y = Integer.parseInt(inputs[2]);
+
+        // Proof
+        System.out.println("x: " + x);
+        System.out.print("y: " + y);
+    }
+
+    // Fake the Jevois class
+    private class Jevois {
+        public String readString() {
+            return "T2 666 -666";
+        }
+    }
+}
